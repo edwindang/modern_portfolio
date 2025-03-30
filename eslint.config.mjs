00fc8452e12daef0c36ea586a494d2a50ce0feab
edwindang <edang@u.rochester.edu>
2025-03-29 22:37:08 -0500

    eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = {
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-page-custom-font': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'react/jsx-no-undef': 'error',
+  },
+};
 
 export default eslintConfig;
